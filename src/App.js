4c6625818597c63cdf63175be2bb37b41e390dcf
BadaHong <badah@hanyang.ac.kr>
2024-07-20 01:01:54 +0900

    edit: sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Correct imports
 import Flipbook from './Flipbook';
+import CreateTest from './CreateTest';
 import './App.css'; // Ensure this path is correct
 
 function App() {
   return (
-    <div className="App">
-      <Flipbook />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Flipbook />} />
+        <Route path="/create-test" element={<CreateTest />} />
+      </Routes>
+    </Router>
   );
 }
 
