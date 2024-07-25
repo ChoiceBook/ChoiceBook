31d251fb689feb316f5460102ddc3264c76493f4
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-25 16:45:46 +0900

    ✨ Rank Page added

--- src/RankPage.js
+++ src/RankPage.js
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import { useParams } from 'react-router-dom';
-import './PlotPage.css';
-import TestBook from './components/TestBook';
+import { useNavigate, useParams } from 'react-router-dom';
 import HomeButton from './HomeButton';
+import RankList from './components/RankList';
 import RetryButton from './RetryButton';
+import './PlotPage.css'; // Assuming you have a CSS file for styling
 
 const RankPage = () => {
   const { user_id, plot_id } = useParams();
-  const [items, setItems] = useState([]);
-  const [plotTitle, setPlotTitle] = useState(''); // Add state for the plot title
-  const [plotDescription, setPlotDescription] = useState(''); // Add state for the plot title
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const [ranks, setRanks] = useState([]);
 
+  useEffect(() => {
+    // Replace with your actual API endpoint
+    fetch(`http://172.10.7.117/api/plots/${plot_id}/users/${user_id}/allranks`)
+      .then(response => response.json())
+      .then(data => {
+        setRanks(data);
+      })
+      .catch(error => console.error('Error fetching ranks:', error));
+  }, [plot_id, user_id]);
 
   const handleClose = () => {
     navigate('/');
@@ -23,6 +29,7 @@ const RankPage = () => {
       <HomeButton />
       <div className="background">
         <button className="close-button" onClick={handleClose}>X</button>
+        <RankList ranks={ranks} />
         <RetryButton plotId={plot_id} /> 
       </div>
     </div>
